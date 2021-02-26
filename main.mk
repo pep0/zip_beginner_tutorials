f1ffa68959f69906f8942631847992374593f946
pep0 <baschy@msn.com>
2021-02-26 11:51:59 +0100

    clean vcd file

--- main.mk
+++ main.mk
@@ -55,6 +55,7 @@ sudo-prog: $(PROJ).bin
 clean:
 	rm -f $(PROJ).asc $(PROJ).rpt $(PROJ).bin $(PROJ).json $(PROJ).log $(ADD_CLEAN) 
 	rm -f $(SIM_MODULE).log $(SIM_MODULE)
+	rm -f $(SIM_MODULE)trace.vcd
 	rm -f -r obj_dir 
 
 .SECONDARY:
